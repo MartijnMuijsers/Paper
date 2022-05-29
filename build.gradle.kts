c3a4c63910cfb61950fb7af38f7ed45e34a531f2
MartijnMuijsers <martijnmuijsers@live.nl>
2022-05-29 13:12:13 +0200

    Update "Pufferfish-Utils" according to the new patch in Pufferfish, and do the same for the newly added "Pufferfish-Config-and-Command", as well as update build.gradlew.kts for the new Pufferfish usage of Simple-YAML

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,13 @@ subprojects {
     repositories {
         mavenCentral()
         maven(paperMavenPublicUrl)
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
     }
 }
 
